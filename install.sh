df3d9fc195457a01898a835441c6d3046d71587b
Jason Wang <--global>
2019-09-21 22:14:28 +0800

    Recode tmux configutation

--- install.sh
+++ install.sh
@@ -3,5 +3,6 @@
 git clone https://github.com/tmux-plugins/tpm plugins/tpm
 
 ln -s ~/.tmux/tmux.conf ~/.tmux.conf
+ln -s ~/.tmux/tmux.conf.local ~/.tmux.conf.local
 
 echo "run tmux and send \"C-x + I\" to it"
