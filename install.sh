874e176458e1ea641cbe059f1bdd89f137c4129e
Jason Wang <wang_borong@163.com>
2025-07-15 14:32:47 +0800

    Update install script

--- install.sh
+++ install.sh
@@ -2,7 +2,4 @@
 
 git clone https://github.com/tmux-plugins/tpm plugins/tpm
 
-ln -s ~/.tmux/tmux.conf ~/.tmux.conf
-ln -s ~/.tmux/tmux.conf.local ~/.tmux.conf.local
-
 echo "run tmux and send \"C-x + I\" to it"
